e6886ffcdc60148c42b07e6e83aee47924b66d2d
Vince <65380728+Socerest2@users.noreply.github.com>
2021-11-23 19:39:40 -0800

    Add files via upload

---
+++ cakculator/cakculator/Form1.Designer.vb
@@ -0,0 +1,285 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.twen = New System.Windows.Forms.Label()
+        Me.ten = New System.Windows.Forms.Label()
+        Me.five = New System.Windows.Forms.Label()
+        Me.change = New System.Windows.Forms.TextBox()
+        Me.one = New System.Windows.Forms.Label()
+        Me.quart = New System.Windows.Forms.Label()
+        Me.tenc = New System.Windows.Forms.Label()
+        Me.nick = New System.Windows.Forms.Label()
+        Me.penny = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(310, 40)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'twen
+        '
+        Me.twen.AutoSize = True
+        Me.twen.Location = New System.Drawing.Point(33, 211)
+        Me.twen.Name = "twen"
+        Me.twen.Size = New System.Drawing.Size(13, 15)
+        Me.twen.TabIndex = 1
+        Me.twen.Text = "0"
+        '
+        'ten
+        '
+        Me.ten.AutoSize = True
+        Me.ten.Location = New System.Drawing.Point(156, 211)
+        Me.ten.Name = "ten"
+        Me.ten.Size = New System.Drawing.Size(13, 15)
+        Me.ten.TabIndex = 2
+        Me.ten.Text = "0"
+        '
+        'five
+        '
+        Me.five.AutoSize = True
+        Me.five.Location = New System.Drawing.Point(260, 211)
+        Me.five.Name = "five"
+        Me.five.Size = New System.Drawing.Size(13, 15)
+        Me.five.TabIndex = 3
+        Me.five.Text = "0"
+        '
+        'change
+        '
+        Me.change.Location = New System.Drawing.Point(141, 40)
+        Me.change.Name = "change"
+        Me.change.Size = New System.Drawing.Size(100, 23)
+        Me.change.TabIndex = 4
+        '
+        'one
+        '
+        Me.one.AutoSize = True
+        Me.one.Location = New System.Drawing.Point(344, 211)
+        Me.one.Name = "one"
+        Me.one.Size = New System.Drawing.Size(13, 15)
+        Me.one.TabIndex = 5
+        Me.one.Text = "0"
+        '
+        'quart
+        '
+        Me.quart.AutoSize = True
+        Me.quart.Location = New System.Drawing.Point(421, 211)
+        Me.quart.Name = "quart"
+        Me.quart.Size = New System.Drawing.Size(13, 15)
+        Me.quart.TabIndex = 6
+        Me.quart.Text = "0"
+        '
+        'tenc
+        '
+        Me.tenc.AutoSize = True
+        Me.tenc.Location = New System.Drawing.Point(495, 211)
+        Me.tenc.Name = "tenc"
+        Me.tenc.Size = New System.Drawing.Size(13, 15)
+        Me.tenc.TabIndex = 7
+        Me.tenc.Text = "0"
+        '
+        'nick
+        '
+        Me.nick.AutoSize = True
+        Me.nick.Location = New System.Drawing.Point(563, 211)
+        Me.nick.Name = "nick"
+        Me.nick.Size = New System.Drawing.Size(13, 15)
+        Me.nick.TabIndex = 8
+        Me.nick.Text = "0"
+        '
+        'penny
+        '
+        Me.penny.AutoSize = True
+        Me.penny.Location = New System.Drawing.Point(622, 211)
+        Me.penny.Name = "penny"
+        Me.penny.Size = New System.Drawing.Size(13, 15)
+        Me.penny.TabIndex = 9
+        Me.penny.Text = "0"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label9.Location = New System.Drawing.Point(29, 162)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(45, 28)
+        Me.Label9.TabIndex = 10
+        Me.Label9.Text = "$20"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label10.Location = New System.Drawing.Point(141, 162)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(45, 28)
+        Me.Label10.TabIndex = 11
+        Me.Label10.Text = "$10"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label11.Location = New System.Drawing.Point(260, 162)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(34, 28)
+        Me.Label11.TabIndex = 12
+        Me.Label11.Text = "$5"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label12.Location = New System.Drawing.Point(344, 162)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(34, 28)
+        Me.Label12.TabIndex = 13
+        Me.Label12.Text = "$1"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label13.Location = New System.Drawing.Point(412, 162)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(45, 28)
+        Me.Label13.TabIndex = 14
+        Me.Label13.Text = "¢25"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label14.Location = New System.Drawing.Point(482, 162)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(45, 28)
+        Me.Label14.TabIndex = 15
+        Me.Label14.Text = "¢10"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label15.Location = New System.Drawing.Point(551, 162)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(34, 28)
+        Me.Label15.TabIndex = 16
+        Me.Label15.Text = "¢5"
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label16.Location = New System.Drawing.Point(629, 162)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(34, 28)
+        Me.Label16.TabIndex = 17
+        Me.Label16.Text = "¢1"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(433, 40)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 18
+        Me.Button2.Text = "Clear"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(541, 40)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 19
+        Me.Button3.Text = "Exit"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.penny)
+        Me.Controls.Add(Me.nick)
+        Me.Controls.Add(Me.tenc)
+        Me.Controls.Add(Me.quart)
+        Me.Controls.Add(Me.one)
+        Me.Controls.Add(Me.change)
+        Me.Controls.Add(Me.five)
+        Me.Controls.Add(Me.ten)
+        Me.Controls.Add(Me.twen)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents twen As Label
+    Friend WithEvents ten As Label
+    Friend WithEvents five As Label
+    Friend WithEvents change As TextBox
+    Friend WithEvents one As Label
+    Friend WithEvents quart As Label
+    Friend WithEvents tenc As Label
+    Friend WithEvents nick As Label
+    Friend WithEvents penny As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ cakculator/cakculator/Form1.vb
@@ -0,0 +1,59 @@
+﻿Public Class Form1
+    Dim twenbil As Integer
+    Dim tenbil As Integer
+    Dim fivebil As Integer
+    Dim onebil As Integer
+    Dim qu As Integer
+    Dim dime As Integer
+    Dim nickle As Integer
+    Dim changee As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        changee = change.Text
+        'twenty
+        twenbil = changee \ 2000
+        changee -= twenbil * 2000
+        twen.Text = twenbil
+        'ten
+        tenbil = changee \ 1000
+        changee -= tenbil * 1000
+        ten.Text = tenbil
+        'five
+        fivebil = changee \ 500
+        changee -= fivebil * 500
+        five.Text = fivebil
+        'one
+        onebil = changee \ 100
+        changee -= onebil * 100
+        one.Text = onebil
+        'quarter
+        qu = changee \ 25
+        changee -= qu * 25
+        quart.Text = qu
+        'dime
+        dime = changee \ 10
+        changee -= dime * 10
+        tenc.Text = dime
+        'nickle
+        nickle = changee \ 5
+        changee -= nickle * 5
+        nick.Text = nickle
+        'penny
+        penny.Text = changee
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        twen.Text = Nothing
+        ten.Text = Nothing
+        five.Text = Nothing
+        one.Text = Nothing
+        quart.Text = Nothing
+        tenc.Text = Nothing
+        nick.Text = Nothing
+        penny.Text = Nothing
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Close()
+    End Sub
+End Class
